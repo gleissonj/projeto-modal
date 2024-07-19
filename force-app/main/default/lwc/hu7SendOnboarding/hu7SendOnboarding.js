9a9a64036050c2547e307559f9de81366dc98c3e
Gleisson Jesus Evangelista <gleisson020391>
2024-07-19 08:16:46 -0300

    teste

---
+++ force-app/main/default/lwc/hu7SendOnboarding/hu7SendOnboarding.js
@@ -0,0 +1,19 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { CloseActionScreenEvent } from 'lightning/actions';
+
+export default class Hu7SendOnboarding extends LightningElement {
+    @api recordId;
+   @api objectApiName;
+   handleSuccess(e) {
+        // Close the modal window and display a success toast
+        this.dispatchEvent(new CloseActionScreenEvent());
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: 'Opportunity Record Updated!',
+                variant: 'success'
+            })
+        );
+   }
+}
