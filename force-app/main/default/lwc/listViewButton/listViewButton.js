88a46e36b782ff8ad6236c5d7277610a0ce35fd4
Gleisson Jesus Evangelista <gleisson020391>
2024-08-10 16:46:05 -0300

    conversao

--- force-app/main/default/lwc/listViewButton/listViewButton.js
+++ force-app/main/default/lwc/listViewButton/listViewButton.js
@@ -1,7 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import GetContato from '@salesforce/apex/ContactController.GetContato';
 
 export default class ListViewButton extends LightningElement {
+    @track isLoading = false;
+    @track isModalOpen = true;
+    apontamentos;
+    error;
+    enableView = false;
     handleClick(event) {
-        alert("bateuu")
+        alert("bateu")
     }
+
+    GetContato() {
+        
+        GetContato().then((result) => {
+            this.apontamentos = result;
+            this.enableView = true;
+            this.error = undefined;
+           // this.showToast();
+            this.isLoading = false;
+        })
+            .catch((error) => {
+                this.error = error;
+                console.log("error", this.error.body)
+                console.log("error2", this.error.body.message)
+                this.apontamentos = undefined;
+                this.enableView = true;
+                this.isLoading = false;
+            });
+    }
+
 }
