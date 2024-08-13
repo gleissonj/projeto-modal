e1bff1f2b4dc9b7074afc8acb17e83df41ef0f71
Gleisson Jesus Evangelista <gleisson020391>
2024-08-13 09:20:22 -0300

    primeiro teste

---
+++ force-app/main/default/lwc/teste1/teste1.js
@@ -0,0 +1,58 @@
+import { LightningElement, track, api} from 'lwc';
+import getApontamento from '@salesforce/apex/ApontamentoController.getApontamento';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class Teste1 extends LightningElement { @track isLoading = false;
+    @track isModalOpen = true;
+    _apontamentos;
+    error;
+    enableView = false;
+
+    @api
+    get apontamentos(){
+        return this._apontamentos;
+    }
+
+    set apontamentos(value){
+        this._apontamentos = value ? [...value] : [];
+    }
+
+    @api
+    getApontamentoCadastrais() {
+        this.isLoading = true;
+        getApontamento().then((result) => {
+            this._apontamentos = result;
+            this.enableView = true;
+            this.error = undefined;
+            this.showToast();
+            this.isLoading = false;
+        })
+            .catch((error) => {
+                this.error = error;
+                console.log("error", this.error.body)
+                console.log("error2", this.error.body.message)
+                this._apontamentos = undefined;
+                this.enableView = true;
+                this.isLoading = false;
+            });
+    }
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Sucesso!',
+            message: 'The operation was completed successfully.',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(event);
+    }
+
+    refreshPage() {
+        // Reloads the entire page
+        window.location.reload();
+    }
+
+    closeModal() {
+        this.isModalOpen = false;
+    }
+}
