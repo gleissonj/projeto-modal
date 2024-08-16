21f8bb2d018b2fa8bde49f6afda95bc9769e4ccf
Gleisson Jesus Evangelista <gleisson020391>
2024-08-16 08:52:34 -0300

    ajuste

--- force-app/main/default/lwc/listViewButton/listViewButton.js
+++ force-app/main/default/lwc/listViewButton/listViewButton.js
@@ -1,19 +1,15 @@
 import { LightningElement, track } from 'lwc';
-import GetContato from '@salesforce/apex/ContactController.GetContato';
+import getContato from '@salesforce/apex/ContactController.GetContato';
 
 export default class ListViewButton extends LightningElement {
     @track isLoading = false;
-    @track isModalOpen = true;
-    apontamentos;
-    error;
-    enableView = false;
-    handleClick(event) {
-        alert("bateu")
-    }
+    @track data;
 
-    GetContato() {
-        
-        GetContato().then((result) => {
+    // Simulate data loading
+    handleLoadData() {
+        this.isLoading = true;
+        this.data = null;
+              GetContato().then((result) => {
             this.apontamentos = result;
             this.enableView = true;
             this.error = undefined;
@@ -29,5 +25,33 @@ export default class ListViewButton extends LightningElement {
                 this.isLoading = false;
             });
     }
+    // @track isLoading = false;
+    // @track isModalOpen = true;
+    
+    // apontamentos;
+    // error;
+    // enableView = false;
+    // handleClick(event) {
+    //     alert("bateu")
+    // }
+
+    // GetContato() {
+        
+    //     GetContato().then((result) => {
+    //         this.apontamentos = result;
+    //         this.enableView = true;
+    //         this.error = undefined;
+    //        // this.showToast();
+    //         this.isLoading = false;
+    //     })
+    //         .catch((error) => {
+    //             this.error = error;
+    //             console.log("error", this.error.body)
+    //             console.log("error2", this.error.body.message)
+    //             this.apontamentos = undefined;
+    //             this.enableView = true;
+    //             this.isLoading = false;
+    //         });
+    // }
 
 }
