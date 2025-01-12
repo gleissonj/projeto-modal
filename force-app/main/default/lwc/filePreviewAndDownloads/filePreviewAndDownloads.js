754e5f568081dda7dc95923426ad9b26d13bda7c
Gleisson Jesus Evangelista <gleisson020391>
2025-01-12 17:07:20 -0300

    criacao preview e donwload

--- force-app/main/default/lwc/filePreviewAndDownloads/filePreviewAndDownloads.js
+++ force-app/main/default/lwc/filePreviewAndDownloads/filePreviewAndDownloads.js
@@ -1,51 +1,35 @@
-import { LightningElement, api } from 'lwc';
-import uploadFile from '@salesforce/apex/filePreviewAndDownloadController.uploadFile';
+import { LightningElement, api, wire } from 'lwc';
+import getRelatedFilesByRecordId from '@salesforce/apex/filePreviewAndDownloadController.getRelatedFilesByRecordId';
+import {NavigationMixin} from 'lightning/navigation'  
 
-export default class FilePreviewAndDownloads extends LightningElement {
-    @api
-    myRecordId;
-    file;
+export default class FilePreviewAndDownloads extends NavigationMixin(LightningElement) {
+   @api recordId
+   filesList=[]
+   @wire(getRelatedFilesByRecordId, {recordId: '$recordId'})
+   wiredResult({data, error}){
+     if(data){
+       console.log('data: ', JSON.stringify(data));
+       this.filesList = Object.keys(data).map(item=>({"label":data[item],
+        "value": item,
+        "url":`/sfc/servlet.shepherd/document/download/${item}`
+       }))
+       console.log('data: ', JSON.stringify(this.filesList));
+     }
+     else if(error){
+       console.log('error: ', JSON.stringify(error));
+     }
+   }
 
-    // EXEMPLO 1
-    // get acceptedFormats() {
-    //     return ['.pdf', '.png'];
-    // }
-
-    // handleUploadFinished(event) {
-    //     // Get the list of uploaded files
-    //     const uploadedFiles = event.detail.files;
-    //     // eslint-disable-next-line no-alert
-    //     alert('No. of files uploaded :' + uploadedFiles.length);
-        
-    // }
-
-    // EXEMPLO 2
-    
-    handleFileUpload(event) {
-        this.file = event.target.files[0];
-    }
-
-    uploadFile() {
-        if (!this.file) {
-        // eslint-disable-next-line no-alert
-            return alert('No file selected');
+   previewHandler(event){
+    console.log(event.target.dataset.id)
+    this[NavigationMixin.Navigate]({ 
+        type:'standard__namedPage',
+        attributes:{ 
+            pageName:'filePreview'
+        },
+        state:{ 
+            selectedRecordId: event.target.dataset.id
         }
-        const reader = new FileReader();
-        reader.onload = () => {
-            const fileContents = reader.result.split(',')[1];
-            const fileName = this.file.name;
-            uploadFile({ fileName, fileContents })
-                .then(result => {
-                    // eslint-disable-next-line no-alert
-                    alert('File uploaded successfully', result);
-                    // Handle success
-                })
-                .catch(error => {
-                    console.error('Error uploading file: ', error);
-                    // Handle error
-                });
-        };
-        reader.readAsDataURL(this.file);
-        return true; // Return a consistent value
-    }
+    })
+}
 }
